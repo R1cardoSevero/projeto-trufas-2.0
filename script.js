3ad8ab4a9116aef5b9a1f13c85dadbdc8dc41fdb
R1cardoSevero <ricardomulersevero@gmail.com>
2025-03-10 03:58:07 -0300

    carrinho de compras criado, adicionado diversas funcionalidas e estilos

--- script.js
+++ script.js
@@ -53,10 +53,10 @@ document.addEventListener('DOMContentLoaded', function () {
             </article>
             <article id="menu-pedido">
                 <h3>${name}</h3>
-                <form action="" method="get">
+                <div>
                     <select name="tamanho" id="tamanho">
-                        <option value="Mini">Trufa Mini</option>
-                        <option value="Trufa">Trufa</option>
+                        <option value="Trufa Mini" data-peso="45">Trufa Mini</option>
+                        <option value="Trufa" data-peso="70">Trufa</option>
                     </select>
                     <select name="casca" id="casca">
                         <option value="Chocolate Meio Amargo">Chocolate Meio Amargo</option>
@@ -64,19 +64,21 @@ document.addEventListener('DOMContentLoaded', function () {
                         <option value="Chocolate ao Leite">Chocolate ao Leite</option>
                         <option value="Chocolate Misto">Chocolate Misto ( Ao Leite + Meio Amargo )</option>
                     </select>
-                    <button type="submit">Colocar no Carrinho</button>
-                </form>
+                    <input type="number" name="quantidade" id="quantidade" min="1" step="1" value="1">
+                    <button onClick="pegarPedido(this)">Colocar no Carrinho</button>
+                </div>
             </article>
         </div>`;
 
+
         var menu = document.getElementById('menu-pedido-container');       
         menu.addEventListener('click', function() {
-            closeOpenPedido();
+            closePedido();
         });
 
         // Impede a propagação do clique no formulário
-        var form = menu.querySelector('form');
-        form.addEventListener('click', function(event) {
+        var div = menu.querySelector('div');
+        div.addEventListener('click', function(event) {
             event.stopPropagation();
         });
 
@@ -87,8 +89,65 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    const carrinho = document.getElementById('carrinho');
+    var lista = carrinho.querySelector('ul');
+    const conteudoSalvo = localStorage.getItem('carrinhoConteudo');
+    if (conteudoSalvo) {
+        lista.innerHTML = conteudoSalvo; // Restaura o conteúdo salvo
+    }
 
-function closeOpenPedido() {
+});
+
+function pegarPedido(botao) {
+    const menu = botao.closest('#menu-pedido-container');
+    const img = menu.querySelector('img').src;
+    const sabor = menu.querySelector('h3').textContent;
+    const tamanhoSelected = menu.querySelector('#tamanho');   
+    const tamanho = menu.querySelector('#tamanho').value;  
+    const peso = tamanhoSelected.selectedOptions[0].dataset.peso;
+    const casca = menu.querySelector('#casca').value;
+    const quantidade = menu.querySelector('#quantidade').value; 
+    adicionarAoCarrinho(sabor, tamanho, casca, quantidade, peso, img);
+    closePedido();
+}
+
+function adicionarAoCarrinho(sabor, tamanho, casca, quantidade, peso, img) {
+    const carrinho = document.getElementById('carrinho')
+    var lista = carrinho.querySelector('ul');
+    lista.innerHTML += `
+            <li class="itemCarrinho">
+                <article>
+                    <img src="${img}" style="height:90px;" alt="imagem-produto">
+                    <div>   
+                        <h2>Trufa de ${sabor}</h2>
+                        <h3>${tamanho} - ${peso}</h3>
+                    </div>
+                </article>
+                <article>
+                    <div>
+                        <h2>Unidade</h2>
+                        <h3>R$ 2.50</h3>
+                    </div>
+                    <div>
+                        <h2>Quantidade</h2>
+                        <h3>${quantidade}</h3>
+                    </div>
+                    <div>
+                        <h2>Total</h2>
+                        <h3>R$ 10.00</h3>
+                    </div>
+                </article>
+                <i class="fa-solid fa-xmark" onclick="removeItem(this)"></i>
+            </li>
+    `;
+    //Salvando o conteudo do carrinho no localStorage
+    localStorage.setItem('carrinhoConteudo', lista.innerHTML);
+}
+
+//localStorage.removeItem('carrinhoConteudo');
+
+function closePedido() {
     document.getElementById('menu-pedido-container').remove();
 }
 
@@ -96,4 +155,20 @@ function closeOpenPedido() {
 function removerAcentos(str) {
     //Gerado com IA
     return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+}
+
+function closeOpenCarrinho(){
+    var carrinho = document.getElementById('carrinho');
+    carrinho.classList.toggle('carrinhoOpen');
+}
+
+function uncheckBox(){
+    document.getElementById('checkBox').checked = false;
+}
+
+function removeItem(itemClicked){
+    itemClicked.closest('li').remove();
+    var carrinho = document.getElementById('carrinho');
+    var lista = carrinho.querySelector('ul');
+    localStorage.setItem('carrinhoConteudo', lista.innerHTML);
 }
